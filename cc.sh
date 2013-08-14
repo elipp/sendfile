c0f92f11c04387798f47737c75235c1ecc08ce26
ELIAS MIETTINEN <eliaspmiettinen@gmail.com>
2013-08-14 13:20:03 +0300

    lolz

--- cc.sh
+++ cc.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+set -e
+
 CC='gcc -O2 -Wall'
 
-$CC src/client.c -o client -lssl -lcrypto -lpthread
-$CC src/server.c -o server -lssl -lcrypto -lpthread
+$CC src/timer.c src/client.c -o client -lssl -lcrypto -lpthread
+$CC src/timer.c src/server.c -o server -lssl -lcrypto -lpthread
